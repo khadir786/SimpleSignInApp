e171f6c55554b328b3722fc1305110f8477c1959
Khadir Abdul <khadir786.ka@googlemail.com>
2024-07-07 20:22:54 +0100

    refactored to reduce nesting

--- SignIn/Validation.cs
+++ SignIn/Validation.cs
@@ -4,32 +4,23 @@ public class Validation
 {
     public static bool InputValidate(string? inputUsername, string? inputPassword)
     {
-        if (string.IsNullOrEmpty(inputUsername) || string.IsNullOrEmpty(inputPassword))
-        {
-            Console.WriteLine("Username and/or password is empty!\n");
-            Console.WriteLine("Please enter a username and password that are not empty\n");
-            return false;
-        }
-        return true;
+        if (!string.IsNullOrEmpty(inputUsername) && !string.IsNullOrEmpty(inputPassword)) return true;
+        Console.WriteLine("Username and/or password is empty!\n");
+        Console.WriteLine("Please enter a username and password that are not empty\n");
+        return false;
     }
     public static bool InputValidateUsername(string? inputUsername)
     {
-        if (string.IsNullOrEmpty(inputUsername))
-        {
-            Console.WriteLine("Username is empty!\n");
-            Console.WriteLine("Please enter a username that is not empty\n");
-            return false;
-        }
-        return true;
+        if (!string.IsNullOrEmpty(inputUsername)) return true;
+        Console.WriteLine("Username is empty!\n");
+        Console.WriteLine("Please enter a username that is not empty\n");
+        return false;
     } 
     public static bool InputValidatePassword(string? inputPassword)
     {
-        if (string.IsNullOrEmpty(inputPassword))
-        {
-            Console.WriteLine("Password is empty!\n");
-            Console.WriteLine("Please enter a password that is not empty\n");
-            return false;
-        }
-        return true;
+        if (!string.IsNullOrEmpty(inputPassword)) return true;
+        Console.WriteLine("Password is empty!\n");
+        Console.WriteLine("Please enter a password that is not empty\n");
+        return false;
     }
 }
