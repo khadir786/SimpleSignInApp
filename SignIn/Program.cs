f1a093c454e9eebfb0acbf1dd49eced99b3fea45
Khadir Abdul <khadir786.ka@googlemail.com>
2024-07-07 18:24:56 +0100

    added allotted attempts logic

--- SignIn/Program.cs
+++ SignIn/Program.cs
@@ -12,8 +12,11 @@ Console.WriteLine("Please enter your username and password\n");
 Console.WriteLine("NOTE: You username and password must not be empty!\n");
 
 bool inputCheck = false;
+byte attempts = 5;
 
-while (!inputCheck)
+Console.WriteLine($"You have {attempts} attempts\n");
+
+while (!inputCheck && attempts != 0)
 {
     Console.WriteLine("Username:");
     string? inputUsername = Console.ReadLine();
@@ -23,6 +26,10 @@ while (!inputCheck)
 
     //Console.WriteLine($"You have entered: {inputUsername} and {inputPassword}");
 
+    // checks if username or password are not a valid input,
+    // if not, then checks if username and password are correct
+    // if yes, loop condition is broken
+    // otherwise, it is a bad attempt and have one less attempt
     if (string.IsNullOrEmpty(inputUsername) || string.IsNullOrEmpty(inputPassword))
     {
         Console.WriteLine("Username and/or password is empty!\n");
@@ -36,14 +43,15 @@ while (!inputCheck)
     {
         Console.WriteLine("\nUsername or password is incorrect");
         Console.WriteLine("Please try again\n");
+        attempts--;
+        Console.WriteLine($"You have {attempts} attempts left");
     }
 }
 
-Console.WriteLine("Access granted!");
+// Message depending on if user inputs correct password or used all allotted attempts
+Console.WriteLine(attempts <= 0 ? "You have used all of your attempts and are now locked out." : "Access granted!");
 
 
-// Notes: Could have an internal attempt counter?
-//          Where the user is locked out after a certain amount of tries
 
 
 
