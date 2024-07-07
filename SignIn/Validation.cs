e0d70356b08a466f5ee10638d3ae8c5247dc98f1
Khadir Abdul <khadir786.ka@googlemail.com>
2024-07-07 20:18:42 +0100

    created a Validation class and static validation method

---
+++ SignIn/Validation.cs
@@ -0,0 +1,15 @@
+﻿namespace SignIn;
+
+public class Validation
+{
+    public static bool InputValidate(string? inputUsername, string? inputPassword)
+    {
+        if (string.IsNullOrEmpty(inputUsername) || string.IsNullOrEmpty(inputPassword))
+        {
+            Console.WriteLine("Username and/or password is empty!\n");
+            Console.WriteLine("Please enter a username and password that are not empty\n");
+            return false;
+        }
+        return true;
+    }
+}
